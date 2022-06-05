d8221af669b68865a046f09cc2653b0f8db7b05f
eduardohor <eduardo.hor@outlook.com>
2022-06-04 23:55:02 -0300

    adding everything

---
+++ telas/editar.php
@@ -0,0 +1,12 @@
+<h1>Editar Contato</h1>
+
+<hr>
+
+<form action="" method="post">
+    <input name="nome" class="form-control mb-2" type="text" placeholder="Nome">
+    <input name="email" class="form-control mb-2" type="text" placeholder="Email">
+    <input name="telefone" class="form-control mb-3" type="text" placeholder="Telefone">
+
+    <button class="btn btn-primary">Enviar</button>
+
+</form>
